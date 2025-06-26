7ca525e2669d3b02c047581282c944c9a3bd8a7e
persevere76 <2024225864@qq.com>
2025-06-26 14:28:48 +0800

    创建了java项目的基本结构和配置文件

---
+++ medical-qna-system/src/main/java/com/medical/qna/medical_qna_system/MedicalQnaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.medical.qna.medical_qna_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalQnaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalQnaSystemApplication.class, args);
+	}
+
+}
---
+++ medical-qna-system/src/test/java/com/medical/qna/medical_qna_system/MedicalQnaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.qna.medical_qna_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalQnaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
