f13c94323d4c0ec61c51f6aabf667a6c139e75f5
moon1visiter <2461319694@qq.com>
2025-06-30 10:51:27 +0800

    基本页面实现

---
+++ medical-qna-vite/src/components/common/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <header style="background: #eee; padding: 1rem;">
+    <nav>
+      <router-link to="/">首页</router-link> |
+      <router-link to="/login">登录</router-link> |
+      <router-link to="/register">注册</router-link> |
+      <router-link to="/history">历史记录</router-link>
+    </nav>
+  </header>
+</template>
---
+++ medical-qna-vite/src/pages/LoginPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>登录页面</h1>
+    <LoginForm />
+  </div>
+</template>
+
+<script setup>
+import LoginForm from '../components/auth/LoginForm.vue';
+</script>
