9fbd19285675cc8788f8cd008007c4cf5e182f59
42techs <3248129749@qq.com>
2025-06-30 14:11:40 +0800

    完善代码

--- medical-qna-system/src/main/java/com/medical/qna/medical_qna_system/service/AuthService.java
+++ medical-qna-system/src/main/java/com/medical/qna/medical_qna_system/service/AuthService.java
@@ -10,8 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import jakarta.annotation.PostConstruct;
 
 @Service
 @Transactional
