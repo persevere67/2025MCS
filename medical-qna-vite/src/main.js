5fb406558d50bea6fb919eccccad32954ce3e9b6
moon1visiter <2461319694@qq.com>
2025-07-01 11:17:44 +0800

    小配置

--- medical-qna-vite/src/main.js
+++ medical-qna-vite/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router/index';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8080'
+axios.defaults.baseURL = 'http://10.242.30.61:8080'
 axios.interceptors.request.use(config => {
     const token = localStorage.getItem('token');
   if (token) {
