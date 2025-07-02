32af8c5a398c7c3c365c81ec276833cf131a365b
42techs <3248129749@qq.com>
2025-07-02 15:03:19 +0800

    将前端router改为hash模式

--- medical-qna-vite/src/router/index.js
+++ medical-qna-vite/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../components/auth/HomePage.vue';
 import DrugDetailPage from '../components/user/DrugDetailPage.vue';
 import QAPage from '../components/user/QAPage.vue';
@@ -14,7 +14,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
