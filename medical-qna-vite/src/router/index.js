9e882bbc11fe7d982a55ee670a1d6dc52819a396
moon1visiter <2461319694@qq.com>
2025-07-02 11:56:54 +0800

    qna01

--- medical-qna-vite/src/router/index.js
+++ medical-qna-vite/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../components/auth/HomePage.vue';
-import HistoryPage from '../components/user/HistoryPage.vue';
 import DrugDetailPage from '../components/user/DrugDetailPage.vue';
 import QAPage from '../components/user/QAPage.vue';
 import Authcontainer from '../components/auth/Authcontainer.vue';
@@ -8,7 +7,6 @@ import AdminPage from '../components/admin/AdminPage.vue';
 
 const routes = [
   { path: '/', component: HomePage, alias: '/home' },
-  { path: '/history', component: HistoryPage },
   { path: '/drugs/:drugId', component: DrugDetailPage },
   { path: '/qna', component: QAPage },
   { path: '/auth', component: Authcontainer },
